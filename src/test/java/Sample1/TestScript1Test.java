ed2d05a440a33091a06424fb8d9551a27855d722
NKhateeja <khateejaaf@gmail.com>
2023-06-01 15:29:07 +0530

    commit by local branch

--- src/test/java/Sample1/TestScript1Test.java
+++ src/test/java/Sample1/TestScript1Test.java
@@ -4,7 +4,7 @@ public class TestScript1Test {
 
 	public static void main(String[] args) {
 		
-		//welcome to script one
+		//welcome as new branch
 		System.out.println("script 1");
 		//thank you by github
 	}
