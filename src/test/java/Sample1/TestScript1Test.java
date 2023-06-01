8fa090b2917a39f4e503b0e792f2d79458b9af52
NKhateeja <khateejaaf@gmail.com>
2023-06-01 15:17:23 +0530

    commit 1

---
+++ src/test/java/Sample1/TestScript1Test.java
@@ -0,0 +1,10 @@
+package Sample1;
+
+public class TestScript1Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("script 1");
+	}
+
+}
---
+++ src/test/java/Sample1/TestScript2Test.java
@@ -0,0 +1,10 @@
+package Sample1;
+
+public class TestScript2Test {
+
+	public static void main(String[] args) {
+
+		System.out.println("script 2");
+	}
+
+}
